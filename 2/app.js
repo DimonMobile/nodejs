80e79b0e3186149b6784973c6eed30366926cda0
Dmitry Plotnikov <d.plotnikov@itransition.com>
2019-09-03 19:25:24 +0300

    lab2: task 5 complete

--- 2/app.js
+++ 2/app.js
@@ -9,7 +9,8 @@ const server = http.createServer((request, response) => {
         response.end("<a href='/html'>/html</a><br>" + 
                      "<a href='/png'>/png</a><br>" + 
                      "<a href='/api/name'>/api/name</a><br>" + 
-                     "<a href='/xmlhttprequest'>/xmlhttprequest</a><br>");
+                     "<a href='/xmlhttprequest'>/xmlhttprequest</a><br>" +
+                     "<a href='/fetch'>/fetch</a><br>");
     } else if (request.url == '/html') {
         let html = fs.readFileSync("./index.html");
         response.writeHead(200, {'Content-type': 'text/html'});
@@ -21,8 +22,8 @@ const server = http.createServer((request, response) => {
         });
     } else if (request.url == '/api/name') {
         response.writeHead(200, {'Content-type': 'text/plain; charset=utf-8'});
-        response.end('Дмитрий Плотников')
-    } else if (request.url == '/png'){
+        response.end('Дмитрий Плотников');
+    } else if (request.url == '/png') {
         let picturePath = './pic.png';
         fs.stat(picturePath, (err, stat) => {
             if (err) {console.log(err)}
@@ -32,6 +33,10 @@ const server = http.createServer((request, response) => {
                 response.end(jpg, 'binary');
             }
         });
+    } else if (request.url == '/fetch') {
+        let html = fs.readFileSync('./fetch.html');
+        response.writeHead(200, {'Content-type': 'text/html'});
+        response.end(html);
     } else {
         response.writeHead(404, {'Content-type': 'text/html'});
         response.end("<h1>404 Error</h1>")
