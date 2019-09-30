7fe2ba44256df2f6b4f2fb7b926f2317f9775fab
Dimon Mobile <mobik.dimka@gmail.com>
2019-09-30 10:34:02 +0300

    lab05: complete

--- 5/05-01.js
+++ 5/05-01.js
@@ -6,6 +6,10 @@ var url = require('url');
 var port = 5000;
 var db = new data.DB();
 
+var sd = null;
+var sc = null;
+var ss = null;
+
 db.on('GET', (req, res) => {
     res.writeHead(200, {'Content-type': 'application/json'});
     res.end(JSON.stringify(db.select()));
@@ -31,6 +35,10 @@ db.on('PUT', (req, res) => {
     });
 });
 
+db.on('COMMIT', () => {
+    db.commit();
+});
+
 db.on('DELETE', (req, res) => {
     let queryId = url.parse(req.url, true).query.id;
     if (typeof queryId != 'undefined') {
@@ -43,6 +51,48 @@ db.on('DELETE', (req, res) => {
     res.end('ok');
 });
 
+process.stdin.on('readable', () => {
+    let args = process.stdin.read().toString().split(' ');
+    let command = args[0];
+    if (command == 'sd') {
+        let value = parseInt(args[1]);
+        if (sd != null) {
+            clearTimeout(sd);
+            sd = null;
+            console.log('Previous sd was interrupted');
+        }
+        sd = setTimeout(function () {process.exit();}, value * 1000);
+        console.log(`Server will be halted down after ${value} seconds!`);
+    } else if (command == 'sc') {
+        if (args[1] != undefined) {
+            let value = parseInt(args[1]);
+            if (sc != null) {
+                clearInterval(sc);
+                sc = null;
+            }
+            sc = setInterval(() => {db.emit('COMMIT');}, value * 1000);
+        } else {
+            clearInterval(sc);
+            sc = null;
+        }
+    } else if (command == 'ss') {
+        if (args[1] == undefined) {
+            return;
+        } else {
+            let value = parseInt(args[1]);
+            if (ss != null) {
+                clearTimeout(ss);
+                ss = null;
+            }
+            ss = setTimeout(() => {
+                ss = null;
+                console.log(db.getStat());
+            }, value * 1000);
+            console.log("Data statistics collection started!");
+            db.reset();
+        }
+    }
+});
 
 http.createServer((request, response) => {
     const pathName = url.parse(request.url).pathname;
@@ -52,6 +102,9 @@ http.createServer((request, response) => {
         let html = fs.readFileSync('./index.html');
         response.writeHead(200, {'Content-type': 'text/html'});
         response.end(html);
+    } else if (pathName == '/api/ss') {
+        response.writeHead(200, {'Content-type': 'application/json'});
+        response.end(JSON.stringify(db.getResult()));
     }
 }).listen(5000);
 
--- 5/data.js
+++ 5/data.js
@@ -3,10 +3,45 @@ var ee = require('events');
 
 var db_data = [{'id': 1, 'fio': 'Kek Shrek', 'date': '05-07-1002'}];
 function DB() {
-    this.select = () => {return db_data};
+    this.statStarted = Date();
+    this.statCompleted = Date();
+    this.getCount = 0;
+    this.insertCount = 0;
+    this.deleteCount = 0;
+    this.commitCount = 0;
+    this.updateCount = 0;
+
+    this.reset = () => {
+        statStarted = Date();
+        getCount = 0;
+        insertCount = 0;
+        deleteCount = 0;
+        commitCount = 0;
+        updateCount = 0;
+    }
+
+    this.getResult = () => {
+        statCompleted = Date();
+        let result = {
+            'started': statStarted,
+            'completed': statCompleted,
+            'getCount': getCount,
+            'insertCount': insertCount,
+            'deleteCount': deleteCount,
+            'updateCount': updateCount
+        };
+        return result;
+    }
+
+    this.getStat = () => {
+        return `Get: ${getCount}, Insert: ${insertCount}, Delete: ${deleteCount}, Commit: ${commitCount}, Update: ${updateCount}`;
+    }
+
+    this.select = () => {++getCount; return db_data;};
     this.insert = (r) => {
         r['id'] = db_data.length + 1;
         db_data.push(r);
+        ++insertCount;
     };
     this.update = (r) => {
         let found = db_data.find(p => p.id == r.id);
@@ -14,10 +49,16 @@ function DB() {
             found.fio = r.fio;
             found.date = r.date;
         }
+        ++updateCount;
     };
     this.delete = (index) => {
         db_data = db_data.filter(p => p.id != index);
+        ++deleteCount;
     };
+    this.commit = () => {
+        console.log("Commited");
+        ++this.commitCount;
+    }
 }
 
 util.inherits(DB, ee.EventEmitter);
