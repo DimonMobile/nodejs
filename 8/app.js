fee533b3d5aa6827295f24edf89a9fbb65fafe46
Dimon Mobile <mobik.dimka@gmail.com>
2019-10-03 17:40:47 +0300

    lab8: 2 task completed

--- 8/app.js
+++ 8/app.js
@@ -13,6 +13,14 @@ let server = http.createServer(function(req, resp) {
             resp.writeHead(200, {'Content-type': 'text/plain'});
             resp.end(`Updated to ${server.keepAliveTimeout}`);
         }
+    } else if (parsedUrl.pathname == '/headers') {
+        resp.writeHead(200, {'Content-type': 'text/plain', 'Custom': 'bla-bla'});
+        let result = '';
+        Object.entries(req.headers).forEach(element => {
+            result += element[0] + ': ' + element[1] + '\n';
+        });
+
+        resp.end(result);
     }
 }).listen(5000);
 
