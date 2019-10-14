309828868a859274efa17ca2077eb263dc1f2c97
Dimon Mobile <mobik.dimka@gmail.com>
2019-10-14 16:03:24 +0300

    lab11: tasks until 4 completed

---
+++ 11/11-03a.js
@@ -0,0 +1,16 @@
+const WebSocket = require('ws');
+
+let ws = new WebSocket('ws://localhost:4000');
+
+ws.on('open', () => {
+    console.log('opened');
+});
+
+ws.on('message', data => {
+    console.log(data);
+});
+
+ws.on('close', () => {
+    console.log('disconnected');
+});
+
