caee8c5a808fe0869dfff8bfda53d01398789f13
Dimon Mobile <mobik.dimka@gmail.com>
2019-10-14 16:16:07 +0300

    lab11: task 4 completed

---
+++ 11/11-04.js
@@ -0,0 +1,12 @@
+const WebSocket = require('ws');
+
+let wss = new WebSocket.Server({port: 4000, host: 'localhost'});
+
+let n = 0;
+wss.on('connection', (ws) => {
+    ws.on('message', data => {
+        let jsonObject = JSON.parse(data);
+        jsonObject.server = ++n;
+        ws.send(JSON.stringify(jsonObject));
+    });
+});
